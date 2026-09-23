4e576de1a6af01f526ce723cbfc195f707436a32
cflynn29-png <cflynn29@setoncatholic.org>
2026-09-23 18:08:23 +0000

    final save

---
+++ cf lab 3.py
@@ -0,0 +1,23 @@
+#addition function
+def add(x,y):
+    print(x+y)
+
+add(3,4)
+
+#subtraction function
+def subtract(x,y):
+    print(x-y)
+
+subtract(3,4)
+
+#multiplication function
+def multiply(x,y):
+    print(x*y)
+
+multiply(3,4)
+
+#division function
+def divide(x,y):
+    print(x/y)
+
+divide(3,4)
