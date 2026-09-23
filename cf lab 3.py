73646fe3650385126bd6c8bd8f680072968d4814
cflynn29-png <cflynn29@setoncatholic.org>
2026-09-23 18:14:40 +0000

    final code

--- cf lab 3.py
+++ cf lab 3.py
@@ -2,22 +2,23 @@
 def add(x,y):
     print(x+y)
 
-add(3,4)
-
 #subtraction function
 def subtract(x,y):
     print(x-y)
-
-subtract(3,4)
 
 #multiplication function
 def multiply(x,y):
     print(x*y)
 
-multiply(3,4)
 
 #division function
 def divide(x,y):
     print(x/y)
 
-divide(3,4)
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
